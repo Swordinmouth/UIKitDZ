0c276951470e44f5d7c47ac6d748a4c2af1b0570
Swordinmouth <valeriy.kantemir@gmail.com>
2021-07-04 13:29:23 +0300

    Полностью исправил замечания Евгения (совы)

--- UIKitDZ/UIKitDZ/TimerViewController.swift
+++ UIKitDZ/UIKitDZ/TimerViewController.swift
@@ -24,54 +24,55 @@ final class TimerViewController: UIViewController {
 
         self.picker.dataSource = self
         self.picker.delegate = self
-        
-    }
 
+    }
 }
 
-//MARK: - extension TimerViewController
-extension TimerViewController: UIPickerViewDelegate,UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 3
-    }
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        switch component {
-        case 0:
-            return 25
-        case 1,2:
-            return 60
-            
-        default:
-            return 0
+//MARK: - extension TimerViewController dataSource
+extension TimerViewController:  UIPickerViewDataSource {
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 3
         }
-    }
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            switch component {
+            case 0:
+                return 25
+            case 1,2:
+                return 60
     
-    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-        return pickerView.frame.size.width/3
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        switch component {
-        case 0:
-            return "\(row) ч"
-        case 1:
-            return "\(row) мин"
-        case 2:
-            return "\(row) с"
-        default:
-            return ""
+            default:
+                return 0
+            }
         }
-    }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        switch component {
-        case 0:
-            hour = row
-        case 1:
-            minutes = row
-        case 2:
-            seconds = row
-        default:
-            break;
-        }
-    }
 }
+
+//MARK: - extension TimerViewController delegate
+extension TimerViewController: UIPickerViewDelegate {
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            switch component {
+            case 0:
+                return "\(row) ч"
+            case 1:
+                return "\(row) мин"
+            case 2:
+                return "\(row) с"
+            default:
+                return ""
+            }
+        }
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            switch component {
+            case 0:
+                hour = row
+            case 1:
+                minutes = row
+            case 2:
+                seconds = row
+            default:
+                break;
+            }
+        }
+    
+}
+
+
